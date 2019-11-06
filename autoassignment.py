ca77aa65a11e638355ba35c7b6676577e2ef0553
Fajri Hanny <fajrihanny@Fajris-MBP.localdomain>
2019-11-06 15:04:49 +0100

    added San Fransico  starting and date time added parentheses for all print statements

--- autoassignment.py
+++ autoassignment.py
@@ -6,15 +6,15 @@ import time
 
 def main():
 	# schedule construction - all in UTC timezone
-	print 'Starting the program .. '
+	print ('Starting the program.. ')
 	startBerlin = datetime.time(6,0,0)
 	endBerlin = datetime.time(14,0,0)
 
 	startAuckland = datetime.time(18,0)
 	endAuckland = datetime.time(2,0)
 
-	startSanFransisco = 
-	endSanFransisco = 
+	startSanFransisco = datetime.time(16,0,0)
+	endSanFransisco = datetime.time(1,0,0)
 
 	# startSaoPaolo = datetime.time(11,0)
 	# endSaoPaolo = datetime.time(19,0)
@@ -27,18 +27,18 @@ def main():
 	headersContentType = {'Authorization':'Basic ZmFqcmkuaGFubnlAY29udGVudGZ1bC5jb20vdG9rZW46dDA4VjVSSEVvSHFIejVNZG9GVmVaYUdZd2J1Mnh0M2FsNTduM0ZsbA==','Content-Type':'application/json'}
 
 	# retrieve last date time assignment for each agent
-	print 'Retrieve all the agents .. '
+	print ('Retrieve all the agents .. ')
 	orderofAgent = []
 	c = conn.cursor()
 	for row in c.execute("SELECT AGENT_ID FROM AGENT_ASSIGNMENT order by LAST_ASSIGNMENT ASC"):
 		orderofAgent.append(row[0])
-	print "Order of agent based on last date time assignment: "
+	print ('Order of agent based on last date time assignment: ')
 	for index in range (0,len(orderofAgent)):
 		print(orderofAgent[index])
 
 
 	# get all agents - make a request to Zendesk
-	print 'Retrieve all available agents .. '
+	print ('Retrieve all available agents .. ')
 	availableAgents = requests.get(availableAgentURL,headers=headers)
 	agentData = availableAgents.json()
 	agentDataString = json.dumps(agentData)
@@ -55,7 +55,7 @@ def main():
 		else:
 			return currentTime >= startTime or currentTime <= endTime
 
-	print 'Retrieve all the schedules available .. '
+	print ('Retrieve all the schedules available .. ')
 	tagSchedule = []
 	if (isTimeBetween(startBerlin,endBerlin)):
 		tagSchedule.append('Berlin')
@@ -64,12 +64,12 @@ def main():
 	if (isTimeBetween(startAuckland,endAuckland)):
 		tagSchedule.append('Auckland')
 
-	print "Available timezone: " 
+	print ('Available timezone: ')
 	for index in range (0,len(tagSchedule)):
 		print(tagSchedule[index])
 
 	# get available agent's ID based on the current time
-	print 'Retrieve all agents availabled for the time zone(s) .. '
+	print ('Retrieve all agents availabled for the time zone(s) .. ')
 	finalAvailableAgent = []
 	for agent in range (0,numberofAgent): # number of agent = 4
 		agentTimezone = str((agentDataDump['results'][agent]['time_zone'])) # get agent's timezone
@@ -78,22 +78,22 @@ def main():
 			if(str(tagSchedule[index]) == agentTimezone):
 				finalAvailableAgent.append(agentDataDump['results'][agent]['id'])
 	
-	print "Available agent based on timezone: " 
+	print ('Available agent based on timezone: ')
 	for index in range (0,len(finalAvailableAgent)):
 		print(finalAvailableAgent[index])
 
 	# check which agent needs to be assigned based on the last assignment time based
-	print 'Getting the final order of the agent .. '
+	print ('Getting the final order of the agent .. ')
 	finalAgentOrder = []
 	for agentOrder in range (0,len(orderofAgent)):
 		if orderofAgent[agentOrder] in finalAvailableAgent:
 			finalAgentOrder.append(orderofAgent[agentOrder])
-	print "Final Agent Order: " 
+	print ('Final Agent Order: ')
 	for index in range (0,len(finalAgentOrder)):
 		print(finalAgentOrder[index])
 
 	# Get all the unassigned tickets and assign them to the available agents
-	print 'Getting all the unassigned tickets .. '
+	print ('Getting all the unassigned tickets .. ')
 	response = requests.get(url,headers=headers)
 	newTicket = response.json()
 	newTicketString = json.dumps(newTicket)
@@ -103,27 +103,27 @@ def main():
 	# assigning ticket to available agent
 	lastPosition = 0
 	if(numberOfTickets>0):
-		print 'Number of new unassigned tickets: ' + str(numberOfTickets) + ' tickets' 
-		print 'Assigning tickets to agents .. '
+		print ('Number of new unassigned tickets: ') + str(numberOfTickets) + (' tickets' )
+		print ('Assigning tickets to agents .. ')
 		for ticket in range (0,numberOfTickets):
 			updateTicketURL = 'https://contentful.zendesk.com/api/v2/tickets/'+str(newTicketDump['results'][ticket]['id'])+'.json'
 		 	for agentIndex in range(lastPosition,len(finalAgentOrder)):
-		 		assignedAgent = str(finalAgentOrder[agentIndex])
-		 		payload = {'ticket': {'status':'open', 'comment': {'body':'This ticket is being auto assigned','public':'false','author_id':'25264784308'}, 'assignee_id':assignedAgent}}
-		 		payloadJson = json.dumps(payload)
-		 		updateTicket = requests.put(updateTicketURL,headers=headersContentType, data=payloadJson)
-		 		getCurrentTime = datetime.datetime.utcnow()
-				assignmentTime = getCurrentTime.strftime('%s')
-				assignmentTimeinInt = int(assignmentTime)
-		 		c.execute("UPDATE AGENT_ASSIGNMENT SET LAST_ASSIGNMENT = ? WHERE AGENT_ID = ?", (assignmentTimeinInt,assignedAgent))
-				conn.commit()
-		 		if(agentIndex == len(finalAgentOrder)-1):
-		 			lastPosition = 0
-		 		else:
-		 			lastPosition = agent_to_assign + 1
-		 		break
+				 assignedAgent = str(finalAgentOrder[agentIndex])
+				 payload = {'ticket': {'status':'open', 'comment': {'body':'This ticket is being auto assigned','public':'false','author_id':'25264784308'}, 'assignee_id':assignedAgent}}
+				 payloadJson = json.dumps(payload)
+				 updateTicket = requests.put(updateTicketURL,headers=headersContentType, data=payloadJson)
+				 getCurrentTime = datetime.datetime.utcnow()
+				 assignmentTime = getCurrentTime.strftime('%s')
+				 assignmentTimeinInt = int(assignmentTime)
+				 c.execute("UPDATE AGENT_ASSIGNMENT SET LAST_ASSIGNMENT = ? WHERE AGENT_ID = ?", (assignmentTimeinInt,assignedAgent))
+				 conn.commit()
+				 if(agentIndex == len(finalAgentOrder)-1):
+					 lastPosition = 0
+				 else:
+					 lastPosition = agent_to_assign + 1
+				 break
 	else:
-		print 'No new tickets to assign'
+		print ('No new tickets to assign')
 
 	conn.close()
 	return;
